32d35511666d4d0c35a959edea25ae16ba5fa4c9
Henrique Douglas <henriquedouglas2803@gmail.com>
2023-11-14 20:27:00 -0300

    adiciona cadastro e listagem de pet

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -3,19 +3,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pet;
 use Illuminate\Http\Request;
 
 class PetController extends Controller
 {
-    public function index(){
-        return 'ola laravel';
+    public function index()
+    {
+        try {
+            $pets = Pet::all();
+            return $pets;
+        } catch (\Throwable $th) {
+            return;
+        }
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
 
         $data = $request->all();
-
-        $name = $request->input('name');
+        // Pegar somente um campo $name = $request->input('name');
+        $pet = Pet::create($data);
+        return $pet;
 
     }
 }
