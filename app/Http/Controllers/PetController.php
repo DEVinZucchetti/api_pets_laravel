2b1a6a67f7872d46e2977d2785c055d01e5e081c
Henrique Douglas <henriquedouglas2803@gmail.com>
2023-11-14 21:47:16 -0300

    adiciona crud de pets

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -24,6 +24,34 @@ class PetController extends Controller
         $data = $request->all();
         // Pegar somente um campo $name = $request->input('name');
         $pet = Pet::create($data);
+    }
+
+    public function destroy($id) {
+        $pet = Pet::find($id);
+
+        if(!$pet) return $this->response('Pet não encontrado', null, false, 404);
+
+        $pet->delete();
+        return $this->response('', null, true, 204);
+    }
+
+    public function show($id) {
+        $pet = Pet::find($id);
+
+        if(!$pet) return $this->response('Pet não encontrado', null, false, 404);
+
+        return $this->response('', $pet, true, 200);
+    }
+
+    public function update($id, Request $request) {
+        $data = $request->all();
+
+        $pet = Pet::find($id);
+
+        if(!$pet) return $this->response('Pet não encontrado', null, false, 404);
+
+        $pet->update($data);
+
         return $pet;
 
     }
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('pets', [PetController::class, 'index']);
 Route::post('pets', [PetController::class, 'store']);
+Route::delete('pets/{id}', [PetController::class, 'destroy']);
+Route::get('pets/{id}', [PetController::class, 'show']);
+Route::put('pets/{id}', [PetController::class, 'update']);
+
+/*
+Route::resource('pets', PetController::class)
+  ->only(['index', 'show', 'store', 'update', 'destroy']);
+*/
+
