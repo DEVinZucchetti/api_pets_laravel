4687139b5a6fdc9bff6899f465ebc219124de3a4
Robert Santos <robertsantosti@outlook.com>
2023-11-17 16:40:56 -0300

    Criando rotas para o crud de pessoas

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use App\Http\Controllers\PetController;
 use App\Http\Controllers\RaceController;
+use App\Http\Controllers\PersonsController;
 use Illuminate\Support\Facades\Route;
+
+Route::resource('persons', PersonsController::class)->only(
+  ['index', 'show', 'store', 'update', 'destroy']
+);
 
 Route::get('pets', [PetController::class, 'index']);
 Route::post('pets', [PetController::class, 'store']);
