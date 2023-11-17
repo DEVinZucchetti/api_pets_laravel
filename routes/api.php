db1192b2645b9aa099839b935c04ee0dd663fed7
Robert Santos <robertsantosti@outlook.com>
2023-11-17 17:39:39 -0300

    Smart refactory nas rotas

--- routes/api.php
+++ routes/api.php
@@ -9,17 +9,12 @@ Route::resource('peoples', PeoplesController::class)->only(
   ['index', 'show', 'store', 'update', 'destroy']
 );
 
-Route::get('pets', [PetController::class, 'index']);
-Route::post('pets', [PetController::class, 'store']);
-Route::delete('pets/{id}', [PetController::class, 'destroy']);
-Route::get('pets/{id}', [PetController::class, 'show']);
-Route::put('pets/{id}', [PetController::class, 'update']);
+Route::resource('pets', PetController::class)->only(
+  ['index', 'show', 'store', 'update', 'destroy']
+);
 
-Route::post('races', [RaceController::class, 'store']);
-Route::get('races', [RaceController::class, 'index']);
-
-/*
-Route::resource('pets', PetController::class)
-  ->only(['index', 'show', 'store', 'update', 'destroy']);
-*/
+Route::prefix('races')->group(function () {
+  Route::get('', [RaceController::class, 'index']);
+  Route::post('', [RaceController::class, 'store']);
+});
 
