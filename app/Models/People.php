9a7582899b80b907cf35feb116844cc753ef12b1
Robert Santos <robertsantosti@outlook.com>
2023-11-24 21:25:02 -0300

    criando relacionamentos

--- app/Models/People.php
+++ app/Models/People.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class People extends Model
 {
@@ -11,4 +12,14 @@ class People extends Model
 
     protected $table = 'peoples';
     protected $fillable = ['name', 'cpf', 'contact'];
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(Client::class, 'id', 'people_id');
+    }
+    
+    public function professional(): BelongsTo
+    {
+        return $this->belongsTo(Professional::class, 'id', 'people_id');
+    }
 }
